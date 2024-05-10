575318a39bf5825df382e18a1889f9741607f196
mrmabdullah <mohammodraselmia128@gmail.com>
2024-05-10 07:37:16 +0600

    client site and server site auth, post, get, delete, update work done 58&59

---
+++ client-car-practice/README.md
@@ -0,0 +1,86 @@
+ -----------------
+ setup client site
+ -----------------
+
+1. goto >>>> https://reactrouter.com
+2. goto >>>> https://tailwindcss.com/docs/guides/vite
+3. goto >>>> https://daisyui.com/docs/install
+4. goto >>>> https://console.firebase.google.com
+5. goto >>>> https://vitejs.dev/guide/env-and-mode.html
+6. goto >>>> 
+7. goto >>>> 
+
+ -----------------
+ Page folder file setup
+ -----------------
+Layout
+ >Main
+Pages
+ >SignUp
+  >>SignUp
+ >Login
+  >>Login
+ >Home
+  >>Home
+  >>Banner
+  >>About
+  >>Servicess
+  >>ServicCard
+ >Shared
+  >>Navber
+  >>Footer
+ >CheckOut
+  >>Cheackout
+  >>Bookings
+  >>BookingTable
+Providers
+ >AauthProviders
+Routes
+ >Routes
+ >RrivateRoutes
+
+<!-- .eslintrc.cjs -->  rules: {'react/prop-types': 'off', }
+
+
+
+
+
+
+
+ -----------------
+ setup server site
+ -----------------
+
+1. goto >>>> https://expressjs.com/en/starter/installing.html
+
+2. create index.js and type
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+3. package.json
+"scripts": {
+    "start": "node index.js",
+  },
+
+4. goto >>>> https://www.npmjs.com/package/cors
+5. goto >>>> https://cloud.mongodb.com/v2/6623bee6440aa14866eca3c7#/clusters/connect?clusterId=Cluster0
+6. goto >>>> https://www.npmjs.com/package/dotenv
+7. goto >>>> https://www.mongodb.com/docs/drivers/node/current/usage-examples/find
+8. goto >>>> 
+9. goto >>>> 
+
+ -----------------
+ Page folder file setup
+ -----------------
+
+
+
+
+
---
+++ client-car-practice/src/Pages/Login/Login.jsx
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom';
+import img from '../../assets/images/login/login.svg'
+import { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProviders';
+const Login = () => {
+    const { handleSigInUser } = useContext(AuthContext)
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        handleSigInUser(email, password)
+            .then(() => {
+                // const user = result.user;
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="w-1/2">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card shrink-0 w-full max-w-sm    shadow-2xl bg-base-100 ml-10">
+                    <h1 className="text-3xl font-bold mt-5 text-center">Login</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign In</button>
+                        </div>
+                    </form>
+                    <div className='text-center mb-10'>
+                        <p>Or Sign In with</p>
+                        <div>
+                            <button className='p-3'>Facebook</button>
+                            <button className='p-3'>github</button>
+                            <button className='p-3'>google</button>
+                        </div>
+                        <p>Need an account? <Link to="/signup">Sign Up</Link> </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
